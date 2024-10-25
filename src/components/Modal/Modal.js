8dbfb9ee1ba1e78d9c60f44c0884aa2143e1ba5b
KwonSeami <kwonsam07@daum.net>
2024-10-25 10:24:18 +0900

    :lipstick: 모달 컴포넌트 추가

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,57 +2,52 @@ import './modal.css';
 
 /**
  * 모달 생성기
- * @classList 클래스
- * @id (필수)아이디
- * @title 타이틀
- * @content 본문
- * @buttons 버튼(확인, 취소 등)
+ * @param {Object} options - 옵션 객체
+ * @param {string[]} [options.classList] - 클래스 목록
+ * @param {string} options.id - 아이디 (필수)
+ * @param {string} options.title - 타이틀
+ * @param {string} options.content - 본문
+ * @param {string} options.buttons - 버튼(확인, 취소 등)
  */
-export default class Modal{
-  constructor({
-    classList,
+export default function createModal(
+  {
+    classList = [],
     id,
     title,
     content,
     buttons
-  }) {
-
-    this.classList = classList;
-    this.id = id;
-    this.title = title;
-    this.content = content;
-    this.buttons = buttons;
   }
-  
-  open() {
-    const popup = document.getElementById(this.id);
+) {
+  const open = () => {
+    const popup = document.getElementById(id);
     popup.classList.add('is-active');
     document.getElementById('dimmed').classList.add('is-active');
     popup.setAttribute('aria-hidden', 'false');
     document.body.style.overflow = 'hidden';
-  }
+  };
 
-  close() {
-    const popup = document.getElementById(this.id);
+  const close = () => {
+    const popup = document.getElementById(id);
     popup.classList.remove('is-active');
     document.getElementById('dimmed').classList.remove('is-active');
     popup.setAttribute('aria-hidden', 'true');
     document.body.style.overflow = '';
-  }
+  };
 
-  render() {
-    return `
-      <div class="dimmed" id="dimmed"></div>
-      <div
-        class="layer-popup ${this.classList ? this.classList : ''}"
-        id="${this.id ? this.id : ''}"
-      >
-        ${this.title ? `<h2 class="title">${this.title}</h2>` : ''}
-        ${this.content}
-        <div class="btn-box">
-          ${this.buttons}
-        </div>
+  const render = () => `
+    <div class="dimmed" id="dimmed"></div>
+    <div class="layer-popup ${classList ? classList.join(' ') : ''}" id="${id}">
+      ${title ? `<h2 class="title">${title}</h2>` : ''}
+      ${content}
+      <div class="btn-box">
+        ${buttons}
       </div>
-    `;
-  }
+    </div>
+  `
+
+  return {
+    open,
+    close,
+    render
+  };
 }
