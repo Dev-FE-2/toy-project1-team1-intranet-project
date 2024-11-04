9c594b2b680fe74c0b6b0ee9eef09f43a49e5858
jurilee0 <juri.lee.dev@gmail.com>
2024-11-04 14:08:39 +0900

    :zap: 근태 이력  조회 시 해당 유저에 해당하는 기록만 노출 #71

--- src/utils/fetchCurrentUserData.js
+++ src/utils/fetchCurrentUserData.js
@@ -5,7 +5,7 @@ import { DB, AUTH } from '../../firebaseConfig';
 /**
  * 현재 로그인한 유저 데이터 가져오기 함수
  * @example const USER_DATA = await fetchCurrentUserData();
-*/
+ */
 export const fetchCurrentUserData = async () => {
   const USER = await new Promise((resolve, reject) => {
     const unsubscribe = onAuthStateChanged(AUTH, USER => {
@@ -21,7 +21,7 @@ export const fetchCurrentUserData = async () => {
   // 인증된 사용자의 데이터 가져오기
   const USER_DOC = await getDoc(doc(DB, 'users', USER.uid));
   if (USER_DOC.exists()) {
-    return USER_DOC.data();
+    return { ...USER_DOC.data(), uid: USER.uid }; // UID 포함해서 반환
   } else {
     throw new Error('유저 데이터가 존재하지 않습니다.');
   }
