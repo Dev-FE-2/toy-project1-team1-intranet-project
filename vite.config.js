103191960073e00f0fc84d1c775c346e39186e96
jurilee0 <juri.lee.dev@gmail.com>
2024-11-07 10:38:56 +0900

    :hammer: 배포 시나리오 공유 - vite 롤업옵션 / deploy 명령 추가 #78

--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,26 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@utils': path.resolve(__dirname, './src/utils'),
       '@pages': path.resolve(__dirname, './src/pages/front'),
       '@adminPages': path.resolve(__dirname, './src/pages/admin'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+          if (id.includes('src/pages')) {
+            const dirs = id.split('/');
+            return dirs[dirs.indexOf('pages') + 1];
+          }
+        },
+      },
     },
   },
 });
